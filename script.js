386103e9cead0ce8f40a042bbe335ad0794687d9
Gabriel Almeida <gabriel.santos190209@gmail.com>
2025-07-31 21:09:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("botao");
+    const box = document.getElementById("caixa");
+
+    botao.addEventListener("click", function() {
+        caixa.value = "";
+    })
+})
